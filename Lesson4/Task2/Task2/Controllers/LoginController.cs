cc6b292e515dc322760a1f226cd206e3cbcc162b
RadSt <desan1986@gmail.com>
2016-03-20 00:57:33 +0300

    End les4 task2 but need some bootstrap css changes

---
+++ Lesson4/Task2/Task2/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task2.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult AuthorizateUser(string txtLogin, string txtPassword)
+        {
+            string path = Server.MapPath("~/Content/Login.txt");
+            using (StreamWriter stWriter=new StreamWriter(path,false,System.Text.Encoding.Default))
+            {
+                stWriter.WriteLine("Login {0} Password {1}",txtLogin,txtPassword);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Lesson4/Task2/Task2/Controllers/RegisterController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task2.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser(string txtLogin, string txtPassword, string txtPassConfirm, string txtEmail)
+        {
+            string path = Server.MapPath("~/Content/Register.txt");
+            using (StreamWriter stWriter=new StreamWriter(path,false,System.Text.Encoding.Default))
+            {
+                stWriter.WriteLine("Login {0} Password {1} Password Confirm {2} Email {3}",
+                    txtLogin,txtPassword,txtPassConfirm,txtEmail);
+            }
+
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
