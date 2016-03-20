71628bd187d4b890a3ee6a13186304e2c665a6eb
RadSt <desan1986@gmail.com>
2016-03-20 20:20:53 +0300

    End les 5 task1

---
+++ Lesson5/Task1/Task1/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Calculate(int firstField, int secField, string clcBtn)
+        {
+            int result = 0;
+            
+            try
+            {
+                switch (clcBtn)
+                {
+                    case "+":
+                        result = firstField + secField;
+                        break;
+                    case "-":
+                        result = firstField - secField;
+                        break;
+                    case "*":
+                        result = firstField*secField;
+                        break;
+                    case "/":
+                        result = firstField/secField;
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                ViewBag.Result = e.ToString();
+            }
+
+            ViewBag.Result = result;
+            return View("Index");
+        }
+    }
+}
