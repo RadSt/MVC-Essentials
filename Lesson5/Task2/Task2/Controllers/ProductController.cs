c7837a5785b6530cdca5ddb1241d66043db8af68
RadSt <desan1986@gmail.com>
2016-03-20 21:50:05 +0300

    Add Product Model Controller and View Add WebGrid table for ProductList

---
+++ Lesson5/Task2/Task2/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task2.Models;
+
+namespace Task2.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult ProductsList()
+        {
+            Product[] products=new Product[]
+            {
+                new Product{Id=1,Name = "Bread",Description = "Fresh",Price = 200M},
+                new Product{Id=2,Name ="Fish",Description = "Sea",Price=500M},
+                new Product{Id=3,Name = "Oranges",Description = "Dairy",Price = 300M},
+                new Product{Id=4,Name ="Apples",Description = "Fruits",Price = 600M},
+                new Product{Id=5,Name = "Cake",Description = "With Cream",Price = 50M} 
+            };
+            return View(products);
+        }
+    }
+}
