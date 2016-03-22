6a21aa7cd7be6ff0291b0f83824f977149e70d67
RadSt <desan1986@gmail.com>
2016-03-22 21:35:06 +0300

    Add Models Enteties

--- Lesson6/Task3/Task3/Models/User.cs
+++ Lesson6/Task3/Task3/Models/User.cs
@@ -1,7 +1,12 @@
-﻿namespace Task3.Models
+﻿using System.Collections.Generic;
+
+namespace Task3.Models
 {
     public class User
     {
-         
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public List<Order> Orders { get; set; }
     }
 }
