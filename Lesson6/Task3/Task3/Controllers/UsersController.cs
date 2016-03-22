97e487e951be7bebc0fccdd7e415f12df8770998
RadSt <desan1986@gmail.com>
2016-03-22 22:28:58 +0300

    Add Controllers and methods

---
+++ Lesson6/Task3/Task3/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task3.Models;
+
+namespace Task3.Controllers
+{
+    public class UsersController : Controller
+    {
+        DatabaseContext db=new DatabaseContext();
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View(db.Users.ToList());
+        }
+
+        public ActionResult Details(int userId)
+        {
+            User user = db.Users.Find(userId);
+            if (user==null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
