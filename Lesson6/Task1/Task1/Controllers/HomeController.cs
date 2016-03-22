ea30959103a520fe4531f917b6da164d98d13184
RadSt <desan1986@gmail.com>
2016-03-22 20:14:30 +0300

    End les 6 task 1

---
+++ Lesson6/Task1/Task1/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Web.Mvc;
+using Task1.Models;
+
+namespace Task1.Controllers
+{
+    public class HomeController : Controller
+    {
+        User users = new User();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(users);
+        }
+
+        [HttpPost]
+        public ActionResult Index(int id, string name, string password)
+        {
+            Debug.WriteLine(id);
+            Debug.WriteLine(name);
+            Debug.WriteLine(password);
+            return View(users);
+        }
+    }
+}
