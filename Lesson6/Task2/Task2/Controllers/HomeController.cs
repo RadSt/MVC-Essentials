738376d2e48597f0bc018b2e9a90335666128738
RadSt <desan1986@gmail.com>
2016-03-22 20:33:55 +0300

    Add les 6 task 2 and phoneModel

---
+++ Lesson6/Task2/Task2/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task2.Models;
+
+namespace Task2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            PhoneNum phoneNum =new PhoneNum(){CountryCode = "123",CityCode = "456",PhoneNumber = "7891011"};
+            return View(phoneNum);
+        }
+    }
+}
---
+++ Lesson6/Task2/Task2/Models/PhoneNum.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace Task2.Models
+{
+    public class PhoneNum
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+        public string CountryCode { get; set; }
+        public string CityCode { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
