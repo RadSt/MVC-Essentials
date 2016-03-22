eba9755fc2acd545c6e0043de784e82555d17ee3
RadSt <desan1986@gmail.com>
2016-03-22 19:58:00 +0300

    Add User model in Project

---
+++ Lesson6/Task1/Task1/Models/UserContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Task1.Models
+{
+    public class UserContext:DbContext
+    {
+        public UserContext()
+            : base("UserConnection")
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
