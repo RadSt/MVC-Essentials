584020e789e7844b9949b501492a592998278312
RadSt <desan1986@gmail.com>
2016-03-23 20:45:15 +0300

    Add User Controller and CRUD methods

--- Lesson6/Task3/Task3/Controllers/UsersController.cs
+++ Lesson6/Task3/Task3/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,7 +17,7 @@ namespace Task3.Controllers
             return View(db.Users.ToList());
         }
 
-        public ActionResult Details(int userId)
+        public ActionResult Details(int userId=0)
         {
             User user = db.Users.Find(userId);
             if (user==null)
@@ -29,5 +30,48 @@ namespace Task3.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            db.Users.Add(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int userId=0)
+        {
+            User user = db.Users.Find(userId);
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(User user)
+        {
+            db.Entry(user).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int userId=0)
+        {
+            User user = db.Users.Find(userId);
+            return View(user);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int userId)
+        {
+            User user = db.Users.Find(userId);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        } 
     }
 }
