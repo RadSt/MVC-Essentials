22e03401e3e7d0f6d51101911481a4d3d66a7a9c
Radik <desan1986@gmail.com>
2016-03-25 16:54:49 +0300

    Add Customers controller

---
+++ Lesson6/Test3Task/Test3Task/Controllers/CostomersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test3Task.Models;
+
+namespace Test3Task.Controllers
+{
+    public class CostomersController : Controller
+    {
+        DatabaseContext db=new DatabaseContext();
+        // GET: Costomers
+        public ActionResult CustomersList()
+        {
+            return View(db.Customers);
+        }
+
+        public ActionResult Details(int customerId = 0)
+        {
+            Customer customer = db.Customers.Find(customerId);
+            return View(customer);
+        }
+    }
+}
--- Lesson6/Test3Task/Test3Task/Controllers/ProductsController.cs
+++ Lesson6/Test3Task/Test3Task/Controllers/ProductsController.cs
@@ -12,7 +12,7 @@ namespace Test3Task.Controllers
     {
         DatabaseContext db=new DatabaseContext();
         // GET: Products
-        public ActionResult Index()
+        public ActionResult ProductsList()
         {
             return View(db.Products);
         }
@@ -33,7 +33,7 @@ namespace Test3Task.Controllers
         {
             db.Products.Add(product);
             db.SaveChanges();
-            return RedirectToAction("Index");
+            return RedirectToAction("ProductsList");
         }
 
         public ActionResult Edit(int productId=0)
@@ -47,7 +47,7 @@ namespace Test3Task.Controllers
         {
             db.Entry(product).State = EntityState.Modified;
             db.SaveChanges();
-            return RedirectToAction("Index");
+            return RedirectToAction("ProductsList");
         }
 
         public ActionResult Delete(int productId=0)
@@ -62,7 +62,7 @@ namespace Test3Task.Controllers
             Product product = db.Products.Find(productId);
             db.Products.Remove(product);
             db.SaveChanges();
-            return RedirectToAction("Index");
+            return RedirectToAction("ProductsList");
         }
 
         protected override void Dispose(bool disposing)
