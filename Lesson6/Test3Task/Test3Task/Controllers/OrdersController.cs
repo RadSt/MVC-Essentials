3d19dc6c1b0510afd2ccee91e8e1fa28a3f3a909
RadSt <desan1986@gmail.com>
2016-03-26 17:00:55 +0300

    Create Orders Controller

---
+++ Lesson6/Test3Task/Test3Task/Controllers/OrdersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test3Task.Models;
+
+namespace Test3Task.Controllers
+{
+    public class OrdersController : Controller
+    {
+        DatabaseContext db=new DatabaseContext();
+        // GET: Orders
+        public ActionResult OrdersList()
+        {
+            return View(db.Orders);
+        }
+    }
+}
