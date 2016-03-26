8ce752081fc27747e10d672742315db9b311f1c3
RadSt <desan1986@gmail.com>
2016-03-26 15:37:10 +0300

    Add missed files

--- Lesson6/Task3/Task3/Controllers/OrdersController.cs
+++ Lesson6/Task3/Task3/Controllers/OrdersController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Task3.Models;
 
 namespace Task3.Controllers
 {
     public class OrdersController : Controller
     {
+        DatabaseContext db=new DatabaseContext();
         // GET: Orders
         public ActionResult Index()
         {
-            return View();
+            return View(db.Order.ToList());
         }
     }
 }
