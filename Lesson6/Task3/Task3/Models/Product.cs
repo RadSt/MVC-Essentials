4237dcc0ba2f03f1ff53c69806e9d43d94f93eb6
RadSt <desan1986@gmail.com>
2016-03-27 14:52:20 +0300

    Start les 6 task 4

--- Lesson6/Task3/Task3/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Task3.Models
-{
-    public class Product
-    {
-        public int ProductId { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-    }
-}
---
+++ Lesson6/Task4/Test3Task/Controllers/OrdersController.cs
@@ -0,0 +1,81 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web.Mvc;
+using Test3Task.Models;
+
+namespace Test3Task.Controllers
+{
+    public class OrdersController : Controller
+    {
+        DatabaseContext db=new DatabaseContext();
+        // GET: Orders
+        public ActionResult OrdersList()
+        {
+            var orders = db.Orders.Include(o => o.Product).Include(o => o.Customer);
+            return View(orders.ToList());
+        }
+
+        public ActionResult Details(int orderId = 0)
+        {
+            var orders = db.Orders.Include(o => o.Product).Include(o => o.Customer);
+            Order order = orders
+                .FirstOrDefault(o => o.OrderId == orderId);
+            return View(order);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.ProductId = new SelectList(db.Products, "ProductId", "Name");
+            ViewBag.CustomerId = new SelectList(db.Customers,"CustomerId","Name");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Order order)
+        {
+            db.Orders.Add(order);
+            db.SaveChanges();
+            return RedirectToAction("OrdersList");
+        }
+
+        public ActionResult Edit(int orderId = 0)
+        {
+            Order order = db.Orders.Find(orderId);
+            ViewBag.ProductId=new SelectList(db.Products,"ProductId","Name");
+            ViewBag.CustomerId = new SelectList(db.Customers, "CustomerId", "Name");
+            return View(order);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Order order)
+        {
+            db.Entry(order).State=EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("OrdersList");
+        }
+
+        public ActionResult Delete(int orderId=0)
+        {
+            var orders = db.Orders.Include(o => o.Product).Include(o => o.Customer);
+            Order order = orders
+                .FirstOrDefault(o => o.OrderId == orderId);
+            return View(order);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public ActionResult DeleteOrder(int orderId)
+        {
+            Order order = db.Orders.Find(orderId);
+            db.Orders.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("OrdersList");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
