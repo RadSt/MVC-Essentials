0080427541bcabbc2918b84e7f649c018cb3f42e
RadSt <desan1986@gmail.com>
2016-03-27 20:18:15 +0300

    Add SalesForCustomer View

--- Lesson6/Task4/Test3Task/Controllers/SalesForCustomerController.cs
+++ Lesson6/Task4/Test3Task/Controllers/SalesForCustomerController.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Test3Task.Models;
 
 namespace Test3Task.Controllers
 {
     public class SalesForCustomerController : Controller
     {
+        DatabaseContext db=new DatabaseContext();
         // GET: SalesForCustomer
         public ActionResult SalesForCustomerReportShow()
         {
-            return View();
+            var orders = db.Orders.Include(o => o.Customer).Include(o => o.Product);
+            ViewBag.Customers = new SelectList(db.Customers, "CustomerId", "Name");
+            return View(orders.ToList());
         }
     }
 }
