8756e47b03512bb23161d14b48c3095a37904a58
RadSt <desan1986@gmail.com>
2016-03-27 15:01:11 +0300

    Add Report Controllers

---
+++ Lesson6/Task4/Test3Task/Controllers/SalesForCustomerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test3Task.Controllers
+{
+    public class SalesForCustomerController : Controller
+    {
+        // GET: SalesForCustomer
+        public ActionResult SalesForCustomerReportShow()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lesson6/Task4/Test3Task/Controllers/SalesForPeriodController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test3Task.Controllers
+{
+    public class SalesForPeriodController : Controller
+    {
+        // GET: SalesForPeriod
+        public ActionResult SalesForPeriodReportShow()
+        {
+            return View();
+        }
+    }
+}
