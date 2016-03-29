42c2c92aced62defa7adae552f646a7e9479ab85
RadSt <desan1986@gmail.com>
2016-03-29 23:27:09 +0300

    Add validation to Customer model

--- Lesson7/Task2/Task2/Models/Customer.cs
+++ Lesson7/Task2/Task2/Models/Customer.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Test3Task.Models
 {
     public class Customer
     {
         public int CustomerId { get; set; }
+         [Required(ErrorMessage = "Введите имя покупателя")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Введите пароль")]
         public string Password { get; set; }
+        [Required(ErrorMessage = "Введите емаил")]
+        [RegularExpression(@"",ErrorMessage = "Введите верную почту")]
         public string Email { get; set; }
         public List<Order> Orders { get; set; }
     }
