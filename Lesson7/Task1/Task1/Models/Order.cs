d93d32870440aa85558f36a4ddcaa4a9199921b0
RadSt <desan1986@gmail.com>
2016-03-29 21:15:27 +0300

    Add Order Model

--- Lesson7/Task1/Task1/Models/Order.cs
+++ Lesson7/Task1/Task1/Models/Order.cs
@@ -1,7 +1,24 @@
-﻿namespace Task1.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace Task1.Models
 {
     public class Order
     {
-         
+
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+        public string Adress { get; set; }
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+        [RegularExpression(@"/.+@.+\..+/i",ErrorMessage = "Введите емаил верного формата")]
+        public DateTime Date { get; set; }
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+        [MustBeTrue(ErrorMesssage="Вы не согласились с условиями использования")]
+        public bool TermsAccepted { get; set; }
     }
 }
