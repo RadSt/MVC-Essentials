c2ed2975792a60e4e3174cfe736ab5832fff533f
RadSt <desan1986@gmail.com>
2016-03-29 21:25:30 +0300

    Add Date Validation in Model

--- Lesson7/Task1/Task1/Models/Order.cs
+++ Lesson7/Task1/Task1/Models/Order.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.Runtime.CompilerServices;
+using System.Web.UI.WebControls;
 
 namespace Task1.Models
 {
@@ -8,14 +9,14 @@ namespace Task1.Models
     {
 
         [Required(ErrorMessage = "Поле обязательно для заполнения")]
-
         public string Name { get; set; }
         [Required(ErrorMessage = "Поле обязательно для заполнения")]
         public string Adress { get; set; }
         [Required(ErrorMessage = "Поле обязательно для заполнения")]
+        [RegularExpression(@"/.+@.+\..+/i", ErrorMessage = "Введите емаил верного формата")]
         public string Email { get; set; }
         [Required(ErrorMessage = "Поле обязательно для заполнения")]
-        [RegularExpression(@"/.+@.+\..+/i",ErrorMessage = "Введите емаил верного формата")]
+        [DataType(DataType.Date, ErrorMessage = "Поле должно быть датой")]
         public DateTime Date { get; set; }
         [Required(ErrorMessage = "Поле обязательно для заполнения")]
         [MustBeTrue(ErrorMessage = "Вы не согласились с условиями использования")]
