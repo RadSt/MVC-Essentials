2ec70d6e0e27a605c3b0d72f7a9bdbbe76b59ad1
Radik <desan1986@gmail.com>
2016-03-31 14:54:01 +0300

    Add Validation in View methods

--- Lesson7/Task2/Task2/Models/Order.cs
+++ Lesson7/Task2/Task2/Models/Order.cs
@@ -6,7 +6,9 @@ namespace Test3Task.Models
     public class Order
     {
         public int OrderId { get; set; }
+        [Required(ErrorMessage = "Выберите продукт")]
         public int ProductId { get; set; }
+        [Required(ErrorMessage = "Выберите покупателя")]
         public int CustomerId { get; set; }
         [Required(ErrorMessage ="Введите дату")]
         [DataType(DataType.Date,ErrorMessage = "Неверный формат даты")]
