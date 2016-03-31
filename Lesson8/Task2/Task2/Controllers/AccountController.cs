53e343df0ae20f7e7feee5a65cdf2b684d8d58b2
RadSt <desan1986@gmail.com>
2016-03-31 22:24:23 +0300

    Add InitializeSimpleMembershipFilter for Login and Roles

--- Lesson8/Task2/Task2/Controllers/AccountController.cs
+++ Lesson8/Task2/Task2/Controllers/AccountController.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Test3Task.Filters;
 
 namespace Test3Task.Controllers
 {
     public class AccountController : Controller
     {
         [Authorize]
-        [InitializeSimpleMembershipAttribute]
+        [InitializeSimpleMembership]
         // GET: Account
         public ActionResult Index()
         {
