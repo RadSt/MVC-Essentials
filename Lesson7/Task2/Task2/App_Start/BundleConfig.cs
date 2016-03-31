7abf4d08fcdc405ce247e988ea57750cf6f1aa34
RadSt <desan1986@gmail.com>
2016-03-31 19:43:42 +0300

    Add Client-side validation. And Make Bundles for all js and css files

---
+++ Lesson7/Task2/Task2/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Optimization;
+
+namespace Test3Task
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+                .Include("~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr")
+                .Include("~/Scripts/medornizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/jqvalidate")
+                .Include("~/Scripts/jquery.unobtrusive*", 
+                "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap")
+                .Include("~/Scripts/bootstrap.js"));
+            bundles.Add(new StyleBundle("~/content/css")
+                .Include("~/Content/site.css","~/Content/bootstrap.css"));
+        }
+    }
+}
--- Lesson7/Task2/Task2/Global.asax.cs
+++ Lesson7/Task2/Task2/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace Test3Task
@@ -13,6 +14,7 @@ namespace Test3Task
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
