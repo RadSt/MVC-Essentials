39c91a4869fdd49dc40a9329ee9c77d8999c83d0
RadSt <desan1986@gmail.com>
2016-03-31 21:48:49 +0300

    Add Account Controller and SimpleMembershipAttribute for user Login

---
+++ Lesson8/Task2/Task2/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test3Task.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Authorize]
+        [InitializeSimpleMembershipAttribute]
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lesson8/Task2/Task2/Filters/InitializeSimpleMembershipAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Test3Task.Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public sealed class InitializeSimpleMembershipAttribute
+    {
+         
+    }
+}
