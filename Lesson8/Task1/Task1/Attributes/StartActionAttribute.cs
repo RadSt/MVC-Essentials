266d279aed3870fdf32fbada93701c6759470284
RadSt <desan1986@gmail.com>
2016-03-31 21:01:39 +0300

    Add Sample ActionFilter Method

---
+++ Lesson8/Task1/Task1/Attributes/StartActionAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Web.Mvc;
+
+namespace Task1.Attributes
+{
+    public class StartActionAttribute : FilterAttribute, IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            WriteMessageToFile(String.Format("Action {0} Time {1}",filterContext.ActionDescriptor.ActionName,DateTime.Now));
+        }
+
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            
+        }
+
+        private void WriteMessageToFile(string message)
+        {
+            string path = "~/App_Data/log.txt";
+            using (StreamWriter file=new StreamWriter(path))
+            {
+                file.WriteLine(message);
+            }
+        }
+    }
+}
