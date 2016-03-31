8979874e413b067e6ec4d03d939b5fe12aa74de9
RadSt <desan1986@gmail.com>
2016-03-31 21:34:37 +0300

    Add Login Model

---
+++ Lesson8/Task2/Task2/Models/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Test3Task.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Введите имя пользователя")]
+        [Display(Name = "Логин")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Введите пароль")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+        [Display(Name="Запомнить меня?")]
+        public bool RememberMe { get; set; }
+    }
+}
