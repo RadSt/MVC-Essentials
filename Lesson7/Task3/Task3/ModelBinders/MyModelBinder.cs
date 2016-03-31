d7f30e5de5960be06c861aaada03f5fb4c67ebca
Radik <desan1986@gmail.com>
2016-03-31 16:57:34 +0300

    Add Web optimization Library in task2. Create ModelBinder in task3.

---
+++ Lesson7/Task3/Task3/ModelBinders/MyModelBinder.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using Task3.Models;
+using IModelBinder = System.Web.ModelBinding.IModelBinder;
+
+namespace Task3.ModelBinders
+{
+    public class MyModelBinder:IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            User user = (User) bindingContext.Model ?? new User();
+
+            bool hasPrefix = bindingContext.ValueProvider.ContainsPrefix(bindingContext.ModelName);
+        }
+    }
+}
