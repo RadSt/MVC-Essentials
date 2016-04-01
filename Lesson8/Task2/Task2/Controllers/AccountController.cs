03060c5655e5b2f85119e9a150030ae5f0d018e4
RadSt <desan1986@gmail.com>
2016-04-01 20:06:16 +0300

    Revert "Add Login View and AutentificationFilter  Programm failed"
    This reverts commit 5de328daf4e9c0696538cf1d854f3e20f115cee4.

--- Lesson8/Task2/Task2/Controllers/AccountController.cs
+++ Lesson8/Task2/Task2/Controllers/AccountController.cs
@@ -3,35 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.UI.WebControls;
 using Test3Task.Filters;
-using Test3Task.Models;
-using WebMatrix.WebData;
 
 namespace Test3Task.Controllers
 {
-    [Authorize]
-    [InitializeSimpleMembership]
     public class AccountController : Controller
     {
-
-        [AllowAnonymous]
-        public ActionResult Login(string returnUrl)
+        [Authorize]
+        [InitializeSimpleMembership]
+        // GET: Account
+        public ActionResult Index()
         {
-            ViewBag.ReturnUrl = returnUrl;
             return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Login(LoginModel model, string returnUrl)
-        {
-            if (ModelState.IsValid && WebSecurity.Login(model.UserName, model.Password, persistCookie:model.RememberMe))
-            {
-                return RedirectToAction(returnUrl);
-            }
-            ModelState.AddModelError("","Введенный пароль или пользователь не существуют");
-            return View(model);
         }
     }
 }
