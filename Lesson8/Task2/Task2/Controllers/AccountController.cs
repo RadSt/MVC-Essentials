a174c74ce8b247a8c01a83d556bc4659a3b0d310
RadSt <desan1986@gmail.com>
2016-04-01 20:26:04 +0300

    Add login Controller Views and Filter

--- Lesson8/Task2/Task2/Controllers/AccountController.cs
+++ Lesson8/Task2/Task2/Controllers/AccountController.cs
@@ -3,17 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcInternetApplication.Filters;
+using Test3Task.Models;
 
 namespace Test3Task.Controllers
 {
+    [Authorize]
+    [InitializeSimpleMembershipAttribute]
     public class AccountController : Controller
     {
-        [Authorize]
-        [InitializeSimpleMembershipAttribute]
-        // GET: Account
-        public ActionResult Index()
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
         {
+            ViewBag.ReturnUrl = returnUrl;
             return View();
+        }
+
+        //
+        // POST: /Account/Login
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model, string returnUrl)
+        {
+            // WebSecurity.Login - аутентифицирует пользователя.
+            // Если логин и пароль введены правильно - метод возвращает значение true после чего выполняет добавление специальных значений в cookies.
+            if (ModelState.IsValid && WebSecurity.Login(model.UserName, model.Password, persistCookie: model.RememberMe))
+            {
+                return RedirectToLocal(returnUrl);
+            }
+
+            // Был введен не правильный логин или пароль
+            ModelState.AddModelError("", "The user name or password provided is incorrect.");
+            return View(model);
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction("OrdersList", "Orders");
+            }
         }
     }
 }
