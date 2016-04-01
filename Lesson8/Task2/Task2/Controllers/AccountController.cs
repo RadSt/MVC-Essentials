b5a1dd7cb4e50445736e9e2c83a10f456c556047
RadSt <desan1986@gmail.com>
2016-04-01 21:50:08 +0300

    Add CreateUser method and View in Account controller

--- Lesson8/Task2/Task2/Controllers/AccountController.cs
+++ Lesson8/Task2/Task2/Controllers/AccountController.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using MvcInternetApplication.Filters;
 using Test3Task.Models;
 using WebMatrix.WebData;
@@ -10,7 +8,7 @@ using WebMatrix.WebData;
 namespace Test3Task.Controllers
 {
     [Authorize]
-    [InitializeSimpleMembershipAttribute]
+    [InitializeSimpleMembership]
     public class AccountController : Controller
     {
         [AllowAnonymous]
@@ -30,7 +28,7 @@ namespace Test3Task.Controllers
         {
             // WebSecurity.Login - аутентифицирует пользователя.
             // Если логин и пароль введены правильно - метод возвращает значение true после чего выполняет добавление специальных значений в cookies.
-            if (ModelState.IsValid && WebSecurity.Login(model.UserName, model.Password, persistCookie: model.RememberMe))
+            if (ModelState.IsValid && WebSecurity.Login(model.UserName, model.Password, model.RememberMe))
             {
                 return RedirectToLocal(returnUrl);
             }
@@ -39,6 +37,46 @@ namespace Test3Task.Controllers
             ModelState.AddModelError("", "The user name or password provided is incorrect.");
             return View(model);
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOff()
+        {
+            //Удаление специальных аунтефикационных cookie значений
+            WebSecurity.Logout();
+
+            return RedirectToAction("OrdersList", "Orders");
+        }
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Попытка создания пользователя
+                try
+                {
+                    //Создание пользователя
+                    WebSecurity.CreateUserAndAccount(model.UserName, model.Password);
+                    //Аутентификация пользователя
+                    WebSecurity.Login(model.UserName,model.Password);
+                    return RedirectToAction("OrdersList", "Orders");
+                }
+                catch (MembershipCreateUserException ex)
+                {
+                    
+                    ModelState.AddModelError("",ErrorCodeToString(ex.StatusCode));
+                }
+            }
+            return View(model);
+        }
+        #region Helpers
 
         private ActionResult RedirectToLocal(string returnUrl)
         {
@@ -46,10 +84,47 @@ namespace Test3Task.Controllers
             {
                 return Redirect(returnUrl);
             }
-            else
+            return RedirectToAction("OrdersList", "Orders");
+        }
+
+        private string ErrorCodeToString(MembershipCreateStatus createStatus)
+        {
+            // See http://go.microsoft.com/fwlink/?LinkID=177550 for
+            // a full list of status codes.
+            switch (createStatus)
             {
-                return RedirectToAction("OrdersList", "Orders");
+                case MembershipCreateStatus.DuplicateUserName:
+                    return "User name already exists. Please enter a different user name.";
+
+                case MembershipCreateStatus.DuplicateEmail:
+                    return "A user name for that e-mail address already exists. Please enter a different e-mail address.";
+
+                case MembershipCreateStatus.InvalidPassword:
+                    return "The password provided is invalid. Please enter a valid password value.";
+
+                case MembershipCreateStatus.InvalidEmail:
+                    return "The e-mail address provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidAnswer:
+                    return "The password retrieval answer provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidQuestion:
+                    return "The password retrieval question provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidUserName:
+                    return "The user name provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.ProviderError:
+                    return "The authentication provider returned an error. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+
+                case MembershipCreateStatus.UserRejected:
+                    return "The user creation request has been canceled. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+
+                default:
+                    return "An unknown error occurred. Please verify your entry and try again. If the problem persists, please contact your system administrator.";   
             }
         }
+
+        #endregion
     }
 }
