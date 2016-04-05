d3cb5aba39328d75794a3128ac79bdb725f6a9ba
Radik <desan1986@gmail.com>
2016-04-05 15:26:25 +0300

    Add AccountsRoleController and Roles methods

---
+++ Lesson8/Task3/Task2/Controllers/AccountsRoleController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Security;
+using Test3Task.Models;
+using WebMatrix.WebData;
+
+namespace Test3Task.Controllers
+{
+    public class AccountsRoleController : Controller
+    {
+        DatabaseContext db=new DatabaseContext();
+        // GET: AccountsRole
+        [Authorize(Roles = "Admin")]
+        public ActionResult AccountsRoleList()
+        {
+            var roles = (SimpleRoleProvider)Roles.Provider;
+            IEnumerable<UserProfile> userProfilesList = db.UserProfiles;
+            List<AccountsRole> userRoles=new List<AccountsRole>();
+
+            foreach (var user  in userProfilesList)
+            {
+                AccountsRole accountsRole=new AccountsRole();
+                accountsRole.UserId = user.UserId;
+                accountsRole.UserName = user.UserName;
+                accountsRole.Role = roles.GetRolesForUser(user.UserName);
+                userRoles.Add(accountsRole);
+            }
+
+
+            return View(userRoles);
+        }
+        [Authorize(Roles = "Admin")]
+        public ActionResult AccountsChangeRole(string userId)
+        {
+            UserProfile user = db.UserProfiles.Find(userId);
+            var roles = (SimpleRoleProvider)Roles.Provider;
+            ViewBag.Roles = new SelectList(roles.GetAllRoles(),"Role");
+            return View(user);
+        }
+    }
+}
---
+++ Lesson8/Task3/Task2/Models/AccountsRole.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Test3Task.Models
+{
+    public class AccountsRole
+    {
+        [Required]
+        [DisplayName("Номер")]
+        public int UserId { get; set; }
+        [Required]
+        [DisplayName("Имя пользователя")]
+        public string UserName { get; set; }
+        [Required]
+        [DisplayName("Роль")]
+        public string[] Role { get; set; }
+    }
+}
