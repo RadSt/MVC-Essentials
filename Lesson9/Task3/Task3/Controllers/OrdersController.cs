57d9e66c7ee40e105a24e89f29abd5c18f8cd3ea
Radik <desan1986@gmail.com>
2016-04-08 10:54:58 +0300

    Start less 9 task 3 add model views controllers

---
+++ Lesson9/Task3/Task3/Controllers/OrdersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task3.Models;
+
+namespace Task3.Controllers
+{
+    public class OrdersController : Controller
+    {
+        IEnumerable<Order> order = OrdersDatabase.Orders;
+        // GET: Default
+        public ActionResult ShowOrders()
+        {
+            return View(order);
+        }
+
+        public ActionResult OrdersData(string id)
+        {
+            order = order.Where(o => o.Customer == id);
+            return PartialView(order);
+        }
+    }
+}
